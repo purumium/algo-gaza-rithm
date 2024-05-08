786a1db9ee2147841112c825ec65f3600d844608
purumium <129027316+purumium@users.noreply.github.com>
2024-05-08 20:09:19 +0900

    [Bronze V] Title: 윤년, Time: 204 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 윤년이면 1, 아니면 0
+		// 윤년은 4의 배수(str % 4 == 0 && str )
+
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		if(num % 4 == 0 && num % 100 != 0 || num % 400 == 0) {
+			System.out.println('1');
+		} else {
+			System.out.println('0');
+		}
+	}
+
+}
