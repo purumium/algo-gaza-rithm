3518cc63869dea2bbfef9c2b39292d6cc4f9ff92
purumium <129027316+purumium@users.noreply.github.com>
2024-05-08 21:31:48 +0900

    두 수 비교하기.java

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -1,17 +1,14 @@
 import java.util.Scanner;
+
 public class Main {
-
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
-		int str1 = sc.nextInt();
-		int str2 = sc.nextInt();
-
-		if(str1 > str2) {
-			System.out.println(">");
-		} else if (str1 < str2 ) {
-			System.out.println("<");
-		} else {
-			System.out.println("==");
-		}
+		int a= sc.nextInt();
+		int b= sc.nextInt();
+	
+		if( a > b ) System.out.println(">");
+		else if ( a < b ) System.out.println("<");
+		else System.out.println("==");
 	}
 }
