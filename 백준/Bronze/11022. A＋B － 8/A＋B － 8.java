9b4f1d7100008543023e32ffccb032829d5f059d
purumium <129027316+purumium@users.noreply.github.com>
2024-05-17 00:20:47 +0900

    [Bronze V] Title: A+B - 8, Time: 176 ms, Memory: 16420 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+			
+			int num = Integer.parseInt( reader.readLine() );  // reader.readLine() 반환타입 = String
+			
+			StringTokenizer stk;
+			for (int i = 1; i <= num; i++) {
+					stk = new StringTokenizer(reader.readLine(), " ");
+					
+					int a = Integer.parseInt( stk.nextToken() );
+					int b = Integer.parseInt( stk.nextToken() );  // 또 다음 토큰이 있으면 출력
+					
+					writer.write( "Case #" + i + ": " + 
+												a + " + "  + b + " = " + ( a + b ) + "\n");
+			}		
+			reader.close();
+			
+			writer.flush();
+			writer.close();			
+	}
+}
